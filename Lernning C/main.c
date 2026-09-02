67b40161b2efd342e78a3c85c3ba73bb1679ecd4
Phiraphob <phiraphob53@gmail.com>
2026-09-02 20:37:33 +0700

    Add files via upload

---
+++ Lernning C/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define Max_valoe 100
+#define Mid_valoe 50
+#define Low_valoe 10
+
+
+int main()
+{
+    printf("Max %d \n",Max_valoe);
+    return 0;
+}
---
+++ Lernning C/main.h
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("Phiraphob");
+    return 0;
+}
