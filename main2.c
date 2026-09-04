3b114c120bedf481855e0d3afff8e462de084549
Phiraphob <phiraphob53@gmail.com>
2026-09-04 19:07:09 +0700

    Add files via upload

---
+++ main2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a1=7;
+    int a2=5;
+
+    printf("Number1 %d\n",a1);
+    printf("Number2 %d\n",a2++);
+    printf("Number3 %d\n",a2);
+    printf("Number4 %d\n",a1--);
+    printf("Number5 %d\n",a1);
+
+    return 0;
+}
